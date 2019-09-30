3dd4ed7dae96597ab6f22a6fd86ba2a728292da2
Srijan Shakya <srijanshakya977@gmail.com>
2019-09-30 14:49:29 +0545

    Updates

---
+++ precache-manifest.f504ee904b1f0c9186a0a9d5f5e6a445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b324347edb047d5b6007a0b6195b41",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "2f022bd3916ad3f58fb7",
+    "url": "/home/static/css/2.2f1c09b2.chunk.css"
+  },
+  {
+    "revision": "a8ee30a7b2f38dcaa180",
+    "url": "/home/static/css/main.cf495e79.chunk.css"
+  },
+  {
+    "revision": "2f022bd3916ad3f58fb7",
+    "url": "/home/static/js/2.f7f540d2.chunk.js"
+  },
+  {
+    "revision": "a8ee30a7b2f38dcaa180",
+    "url": "/home/static/js/main.928de288.chunk.js"
+  },
+  {
+    "revision": "566a489a5a95500f3491",
+    "url": "/home/static/js/runtime~main.7bee73d1.js"
+  }
+]);
